7cc6d6c5aeb74a6de7bfc1ebb7cc7b2b26f28ea6
lukecartledge <luke@lukecartledge.com>
2026-04-30 18:18:07 +0200

    refactor(tooling): add TypeScript configuration and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import typescriptEslint from 'typescript-eslint'
 
 export default [
   js.configs.recommended,
@@ -20,6 +21,23 @@ export default [
       'no-useless-assignment': 'warn',
       'no-control-regex': 'warn',
       'no-useless-catch': 'warn',
+    },
+  },
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      globals: {
+        ...globals.worker,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint.plugin,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off',
     },
   },
   {
